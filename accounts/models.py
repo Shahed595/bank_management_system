21a269d77e124b698dd389e48989376bb7eefbc3
Shahed595 <shahed.ahmed.cse@gmail.com>
2023-08-26 16:08:27 +0600

    creating_custim_user_model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractBaseUser
+from .managers import UserManager
 # Create your models here.
+class User(AbstractBaseUser):
+    username = None
+    email = models.EmailField(unique=True, null=False, blank=True)
+    objects = UserManager()#This will hold all the value of usemanger file as object
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    def __str__(self):
+        return self.email
+    
+    @property#this decorator allow to get the balance of the account
+    def balance(self):
+        if hasattr(self,'account'):
+            return self.account.balance
+        return 0
+    
+    
