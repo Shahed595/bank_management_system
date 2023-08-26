b4c779a13a789123d9b96d5edd0bf5bdf39e3fbe
Shahed595 <shahed.ahmed.cse@gmail.com>
2023-08-26 20:47:06 +0600

    creating-model-for-transactions-app

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,10 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from .views import UserRegistrationView, UserLoginView,LogoutView
 
+
+#app_name = 'accounts'
 urlpatterns = [
-   
+   path('login/', UserLoginView.as_view(), name='user_login'),
+   path('logout/', LogoutView.as_view(), name='user_logout'),
+   path('register/', UserRegistrationView.as_view(), name='user_register'),
 ]
